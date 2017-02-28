6a4e8e4640cd01fa5df16b9124faf3dcd7252730
Alexander Dolgan <5704737@gmail.com>
2017-02-28 11:22:37 +0300

    main work

--- gulpfile.js
+++ gulpfile.js
@@ -1,94 +1,94 @@
 'use strict';
 
 var gulp = require('gulp'),
-    watch = require('gulp-watch'),           //Streaming
+	watch = require('gulp-watch'),           //Streaming
+	gutil = require('gulp-util'),
+	pug = require('gulp-pug'),               // Gulp plugin for compiling Pug templates (old jade)
 
-    pug = require('gulp-pug'),               // Gulp plugin for compiling Pug templates (old jade)
+	sass = require('gulp-sass'),
+	prefixer = require('gulp-autoprefixer'),
+	cleancss = require('gulp-clean-css'),    //Minify css with clean-css.
 
-    sass = require('gulp-sass'),
-    prefixer = require('gulp-autoprefixer'),
-    cleancss = require('gulp-clean-css'),    //Minify css with clean-css.
+	uglify = require('gulp-uglify'),         //Minify files with UglifyJS.
+	sourcemaps = require('gulp-sourcemaps'),
 
-    uglify = require('gulp-uglify'),         //Minify files with UglifyJS.
-    sourcemaps = require('gulp-sourcemaps'),
+	imagemin = require('gulp-imagemin'),      //Minify PNG, JPEG, GIF and SVG images
+	pngquant = require('imagemin-pngquant'),
+	svgSprite = require('gulp-svg-sprite'),  //Create SVG sprites with PNG fallbacks
 
-    imagemin = require('gulp-imagemin'),      //Minify PNG, JPEG, GIF and SVG images
-    pngquant = require('imagemin-pngquant'),
-    svgSprite = require('gulp-svg-sprite'),  //Create SVG sprites with PNG fallbacks
+	rigger = require('gulp-rigger'),          //Rigger is a build time include engine for Javascript,
+											  // CSS, CoffeeScript and in general any type of text file
+											  // that you wish to might want to "include" other files into.
+	plumber = require('gulp-plumber'),        // Prevent pipe breaking caused by errors from gulp plugins
+	rimraf = require('rimraf'),               //The UNIX command rm -rf for node. - clean
+	gutil = require('gulp-util'),             //Utility functions for gulp plugins
+	concat = require('gulp-concat'),          //Concatenates files
+	filter = require("gulp-filter"),          //Filter files in a vinyl stream
+	rename = require('gulp-rename'),          //Rename files
 
-    rigger = require('gulp-rigger'),          //Rigger is a build time include engine for Javascript,
-                                              // CSS, CoffeeScript and in general any type of text file
-                                              // that you wish to might want to "include" other files into.
-    plumber = require('gulp-plumber'),        // Prevent pipe breaking caused by errors from gulp plugins
-    rimraf = require('rimraf'),               //The UNIX command rm -rf for node. - clean
-    gutil = require('gulp-util'),             //Utility functions for gulp plugins
-    concat = require('gulp-concat'),          //Concatenates files
-    filter = require("gulp-filter"),          //Filter files in a vinyl stream
-    rename = require('gulp-rename'),          //Rename files
-
-    browserSync = require("browser-sync"),//Live CSS Reload &amp; Browser Syncing
-    reload = browserSync.reload;
+	browserSync = require("browser-sync"),//Live CSS Reload &amp; Browser Syncing
+	reload = browserSync.reload;
 
 var path = {
-    //export paths
-    build: {
-        fonts: 'build/fonts/',
-        img: 'build/img/',
-        icons: 'src/img/assets/icons/sprites',
-        //html: 'build/',
-        pug: 'build/',
-        js: 'build/js/',
-        css: 'build/style'
-    },
-    //source paths
-    src: {
-        fonts: 'src/fonts/**/*.*',
-        img: 'src/img/**/*.*',
-        icons: 'src/img/assets/icons/*.*',
-        //html: 'src/*.html',
-        pug: 'src/pug/*.pug',
-        js: 'src/js/*.js',
-        css: 'src/style/*.scss'
-    },
-    //whatcher paths
-    watch: {
-        fonts: 'src/fonts/**/*.*',
-        img: 'src/img/**/*.*',
-        icons: 'src/img/assets/icons/*.*',
-        //html: 'src/**/*.html',
-        pug: 'src/pug/**/*.pug',
-        js: 'src/js/**/*.js',
-        css: 'src/style/**/*.scss'
-    },
-    clean: './build',
-    util: 'src/js/util'
+	//export paths
+	build: {
+		fonts: 'build/fonts/',
+		img: 'build/img/',
+		icons: 'src/img/assets/icons/sprites',
+		//html: 'build/',
+		pug: 'build/',
+		js: 'build/js/',
+		css: 'build/style'
+	},
+	//source paths
+	src: {
+		fonts: 'src/fonts/**/*.*',
+		img: 'src/img/**/*.*',
+		icons: 'src/img/assets/icons/*.*',
+		//html: 'src/*.html',
+		pug: 'src/pug/*.pug',
+		js: 'src/js/*.js',
+		css: 'src/style/*.scss'
+	},
+	//whatcher paths
+	watch: {
+		fonts: 'src/fonts/**/*.*',
+		img: 'src/img/**/*.*',
+		icons: 'src/img/assets/icons/*.*',
+		//html: 'src/**/*.html',
+		pug: 'src/pug/**/*.pug',
+		js: 'src/js/**/*.js',
+		css: 'src/style/**/*.scss'
+	},
+	clean: './build',
+	util: 'src/js/util'
 };
 
 //config for the web server
 var config = {
-    server: {
-        baseDir: "./build"
-    },
-    //tunnel: true,
-    host: 'localhost',
-    port: 9000,
-    logPrefix: "heritage"
+	server: {
+		baseDir: "./build"
+	},
+	//tunnel: true,
+	host: 'localhost',
+	port: 9000,
+	logPrefix: "heritage"
 };
 
 
 function onError(err) {
-    console.log(err);
-    this.emit('end');
+	console.log(err);
+	this.emit('end');
 }
 
 gulp.task('pug:build', function () {
-    gulp.src(path.src.pug)
-        .pipe(pug({pretty: true}))
-        .pipe(gulp.dest(path.build.pug))
-        .on('error', function (err) {
-            gutil.log(err.message);
-        })
-        .pipe(reload({stream: true}));
+	gulp.src(path.src.pug)
+		.pipe(pug({pretty: true}))
+		.pipe(gulp.dest(path.build.pug))
+		.on('error', function (err) {
+			gutil.log(err.message);
+		})
+		.pipe(reload({stream: true}));
 
 });
 
@@ -100,48 +100,51 @@ gulp.task('pug:build', function () {
 //});
 
 gulp.task('js:build', function () {
-    gulp.src(path.src.js)
-        .pipe(sourcemaps.init())
-        .pipe(rigger())
-        .pipe(rename({suffix: '.min'}))
-        .pipe(uglify())
-        .pipe(sourcemaps.write('../js'))
-        .pipe(gulp.dest(path.build.js))
-        .on('error', function (err) {
-            gutil.log(err.message);
-        })
-        .pipe(reload({stream: true}));
+	gulp.src(path.src.js)
+		.pipe(sourcemaps.init())
+		.pipe(rigger())
+		.pipe(rename({suffix: '.min'}))
+		.pipe(uglify().on('error', function(err) {
+			gutil.log(gutil.colors.red('[Error]'), err.toString());
+			this.emit('end');
+		}))
+		.pipe(sourcemaps.write('../js'))
+		.pipe(gulp.dest(path.build.js))
+		.on('error', function (err) {
+			gutil.log(err.message);
+		})
+		.pipe(reload({stream: true}));
 });
 
 gulp.task('style:build', function () {
-    gulp.src(path.src.css)
-        .pipe(sourcemaps.init())
-        .pipe(sass().on('error', sass.logError))
-        .pipe(rigger())
-        .pipe(prefixer({
-            browsers: ['last 2 versions'],
-            cascade: false
-        }))
-        .pipe(rename({suffix: '.min'}))
-        // .pipe(cleancss({compatibility: 'ie8'}))
-        .pipe(sourcemaps.write('../style'))
-        .pipe(gulp.dest(path.build.css))
-        .on('error', function (err) {
-            gutil.log(err.message);
-        })
-        .pipe(reload({stream: true}));
+	gulp.src(path.src.css)
+		.pipe(sourcemaps.init())
+		.pipe(sass().on('error', sass.logError))
+		.pipe(rigger())
+		.pipe(prefixer({
+			browsers: ['last 2 versions'],
+			cascade: false
+		}))
+		.pipe(rename({suffix: '.min'}))
+		// .pipe(cleancss({compatibility: 'ie8'}))
+		.pipe(sourcemaps.write('../style'))
+		.pipe(gulp.dest(path.build.css))
+		.on('error', function (err) {
+			gutil.log(err.message);
+		})
+		.pipe(reload({stream: true}));
 });
 
 gulp.task('image:build', function () {
-    gulp.src(path.src.img)
-        .pipe(imagemin({
-            progressive: true,
-            svgoPlugins: [{removeViewBox: false}],
-            use: [pngquant()],
-            interlaced: true
-        }))
-        .pipe(gulp.dest(path.build.img))
-        .pipe(reload({stream: true}));
+	gulp.src(path.src.img)
+		.pipe(imagemin({
+			progressive: true,
+			svgoPlugins: [{removeViewBox: false}],
+			use: [pngquant()],
+			interlaced: true
+		}))
+		.pipe(gulp.dest(path.build.img))
+		.pipe(reload({stream: true}));
 });
 
 
@@ -164,51 +167,51 @@ gulp.task('image:build', function () {
 // });
 
 gulp.task('fonts:build', function () {
-    gulp.src(path.src.fonts)
-        .pipe(gulp.dest(path.build.fonts))
+	gulp.src(path.src.fonts)
+		.pipe(gulp.dest(path.build.fonts))
 });
 
 gulp.task('build', [
-    //'html:build',
-    'pug:build',
-    'js:build',
-    'style:build',
-    'image:build',
-    'fonts:build'
-    // 'svgSprite:build'
+	//'html:build',
+	'pug:build',
+	'js:build',
+	'style:build',
+	'image:build',
+	'fonts:build'
+	// 'svgSprite:build'
 ]);
 
 gulp.task('watch', function () {
-    //watch([path.watch.html], function (event, cb) {
-    //    gulp.start('html:build');
-    //});
-    watch([path.watch.pug], function (event, cb) {
-        gulp.start('pug:build');
-    });
-    watch([path.watch.css], function (event, cb) {
-        gulp.start('style:build');
-    });
-    watch([path.watch.js], function (event, cb) {
-        gulp.start('js:build');
-    });
-    watch([path.watch.img], function (event, cb) {
-        gulp.start('image:build');
-    });
-    // watch([path.watch.icons], function (event, cb) {
-    //     gulp.start('svgSprite:build');
-    // });
-    watch([path.watch.fonts], function (event, cb) {
-        gulp.start('fonts:build');
-    });
+	//watch([path.watch.html], function (event, cb) {
+	//    gulp.start('html:build');
+	//});
+	watch([path.watch.pug], function (event, cb) {
+		gulp.start('pug:build');
+	});
+	watch([path.watch.css], function (event, cb) {
+		gulp.start('style:build');
+	});
+	watch([path.watch.js], function (event, cb) {
+		gulp.start('js:build');
+	});
+	watch([path.watch.img], function (event, cb) {
+		gulp.start('image:build');
+	});
+	// watch([path.watch.icons], function (event, cb) {
+	//     gulp.start('svgSprite:build');
+	// });
+	watch([path.watch.fonts], function (event, cb) {
+		gulp.start('fonts:build');
+	});
 
 });
 
 gulp.task('webserver', function () {
-    browserSync(config);
+	browserSync(config);
 });
 
 gulp.task('clean', function (cb) {
-    rimraf(path.clean, cb);
+	rimraf(path.clean, cb);
 });
 
 gulp.task('default', ['build', 'webserver', 'watch']);
--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -1,32 +1,10 @@
-// var main = function () {
-//     //Toggle vis terms
-//     $('.terms__header').click(function () {
-//         $('.terms__content').toggleClass('is-hide');
-
-//         var tag = $('.terms__header');
-//         $('html,body').animate({scrollTop: tag.offset().top}, 'slow');
-
-//     });
-
-//     //Toggle vis nav on small screen
-//     $('.sec-menu-btn').click(function () {
-//         $('.top-nav').toggleClass('is-show');
-
-//     });
-// };
-
-$(document).ready(function(){
-    // // add fixed property for top-nav on window sctoll down
-    // $(window).scroll(function(){
-    //     var topNavPos = $('top-nav').scrollTop();
-    //     if (topNavPos > 200) {
-    //         $('top-nav').css({
-    //
-    //         });
-    //     } else {
-    //         $('top-nav').css({
-    //
-    //         });
-    //     }
-    // })
+$(document).ready(function() {
+	$(".sub-nav a").click(function(event) {
+		event.preventDefault();
+		$(this).parent().addClass("is-current");
+		$(this).parent().siblings().removeClass("is-current");
+		var tab = $(this).attr("href");
+		$(".l-showcase").not(tab).css("display", "none");
+		$(tab).fadeIn();
+	});
 });
